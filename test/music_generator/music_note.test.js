a9e1b91af12ade7c376dfd3c9ed9aa9e72bd75fa
David Ellis <ducksual@gmail.com>
2019-06-10 19:40:00 +0100

    Initial tests for musicnote

--- test/music_generator/music_note.test.js
+++ test/music_generator/music_note.test.js
@@ -1,5 +1,67 @@
 import { expect } from 'chai'
 import { describe } from 'mocha'
 
-import { keyList} from '../../src/music_theory/keys'
+import { AppConfig } from '../../src/config'
 import { MusicNote } from '../../src/music_generator/music_note'
+
+describe('MusicNote', function () {
+  describe('constructor', function () {
+    it('Should store the inputs in the instance', function () {
+      let config = new AppConfig()
+      let pitch = 60
+      let duration = 'q'
+
+      let note = new MusicNote(pitch, duration, config)
+
+      expect(note.pitch).to.equal(pitch)
+      expect(note.duration).to.equal(duration)
+      expect(note.config).to.equal(config)
+    })
+    it('Should generate elements for VexFlow display', function () {
+      let config = new AppConfig({ clef: 'treble' })
+      let pitch = 60
+      let duration = 'q'
+
+      let note = new MusicNote(pitch, duration, config)
+
+      expect(note.vexElement.keys).to.eql(['C/4'])
+      expect(note.vexElement.duration).to.equal(duration)
+      expect(note.vexElement.clef).to.equal('treble')
+      expect(note.divID).to.include('auto')
+    })
+  })
+  describe('noteName', function () {
+    it('Should return C4 for C4 in the key of C', function () {
+      let config = new AppConfig({ keyName: 'C' })
+      let pitch = 60
+      let duration = 'q'
+      let note = new MusicNote(pitch, duration, config)
+
+      expect(note.noteName).to.equal('C')
+      expect(note.noteRange).to.equal(4)
+      expect(note.keyPitch).to.equal('C/4')
+    })
+    it('Should return B#3 for C4 in the key of C#', function () {
+      let config = new AppConfig({ keyName: 'C#' })
+      let pitch = 60
+      let duration = 'q'
+
+      let note = new MusicNote(pitch, duration, config)
+
+      expect(note.noteName).to.equal('B#')
+      expect(note.noteRange).to.equal(3)
+      expect(note.keyPitch).to.equal('B#/3')
+    })
+    it('Should return Cb4 for B3 in the key of Bb', function () {
+      let config = new AppConfig({ keyName: 'Bb' })
+      let pitch = 59
+      let duration = 'q'
+
+      let note = new MusicNote(pitch, duration, config)
+
+      expect(note.noteName).to.equal('Cb')
+      expect(note.noteRange).to.equal(4)
+      expect(note.keyPitch).to.equal('Cb/4')
+    })
+  })
+})
