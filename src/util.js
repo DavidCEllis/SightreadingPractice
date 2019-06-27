62b9b9fa01d3f75a3fda4e329964e138193bb704
David Ellis <ducksual@gmail.com>
2019-06-27 15:58:59 +0100

    Make the build take tensorflow from cdn

--- src/util.js
+++ src/util.js
@@ -1,9 +1,7 @@
 const moduleToCdn = require('module-to-cdn')
 
 const CDNLinks = {
-  'ml5': { name: 'ml5', var: 'ml5', url: 'https://unpkg.com/ml5@0.2.3/dist/ml5.min.js', version: '0.2.3' },
-  'p5': { name: 'p5', var: 'p5', url: 'https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.8.0/p5.min.js', version: '0.8.0' },
-  // 'p5/lib/addons/p5.sound': { name: 'p5.sound', var: 'p5.sound', url: 'https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.8.0/addons/p5.sound.min.js', version: '0.8.0' }
+  '@tensorflow/tfjs': { name: '@tensorflow/tfjs', var: 'tf', url: "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.2.1/dist/tf.min.js", version: '1.2.1'},
   'vexflow': { name: 'VexFlow', var: 'Vex', url: 'https://unpkg.com/vexflow@1.2.89/releases/vexflow-min.js', version: '1.2.89' }
 }
 
